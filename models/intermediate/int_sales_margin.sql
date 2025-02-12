9e9677a60cc777257e9d11f54135068e4ca0ddcd
Louise Knoery <loutrekne@gmail.com>
2025-02-12 13:19:06 +0000

    DBT Advanced exercice 4

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,12 +1,28 @@
+WITH sales_margin AS (
+
 SELECT
-      products_id,
-      date_date,
-      orders_id,
-      revenue,
-      quantity,
-      purchase_price,
-      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
-      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
-  FROM {{ref("stg_raw__sales")}} s
-  LEFT JOIN {{ref("stg_raw__product")}} p
-      USING (products_id)
+    products_id,
+    date_date,
+    orders_id,
+    revenue,
+    quantity,
+    purchase_price,
+    ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+    ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin,
+FROM {{ref("stg_raw__sales")}} s
+LEFT JOIN {{ref("stg_raw__product")}} p
+USING (products_id)
+
+)
+
+SELECT 
+    products_id
+    ,date_date
+    ,orders_id
+    ,revenue
+    ,quantity
+    ,purchase_price
+    ,purchase_cost
+    ,margin
+    ,{{ margin_percent('revenue', 'purchase_cost') }} AS margin_percent
+FROM sales_margin
