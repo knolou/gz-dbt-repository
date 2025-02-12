8e5b5d73d30d870a9b86d7efcf96f65f5bb41ab8
Louise Knoery <loutrekne@gmail.com>
2025-02-12 15:38:52 +0000

    add campaign month (forgotten)

---
+++ models/mart/finance/finance_campaigns_month.sql
@@ -0,0 +1,21 @@
+SELECT 
+    EXTRACT(MONTH FROM date_date) AS datemonth
+    ,SUM(ads_margin) AS ads_margin
+    ,ROUND(SUM(average_basket),2) AS average_basket
+    ,SUM(operational_margin) AS operational_margin
+    ,SUM(ads_cost) AS ads_cost 
+    ,SUM(ads_impression) AS ads_impression
+    ,SUM(ads_clicks) AS ads_clicks
+    ,SUM(quantity) AS quantity
+    ,SUM(revenue) AS revenue
+    ,SUM(purchase_cost) AS purchase_cost
+    ,SUM(margin) AS margin
+    ,SUM(shipping_fee) AS shipping_fee
+    ,SUM(logcost) AS logcost
+    ,SUM(ship_cost) AS ship_cost
+    
+FROM 
+ {{ref("finance_campaigns_day")}}
+
+GROUP BY datemonth
+ORDER BY datemonth DESC
